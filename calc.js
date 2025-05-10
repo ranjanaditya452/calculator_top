79f84aaf68d81c1d085f47cebc589e5f93a2d42f
Aditya <ranjanaditya452@gmail.com>
2025-05-10 18:51:24 +0530

    Functioning skeleton , needs cleaner JS

--- calc.js
+++ calc.js
@@ -16,6 +16,8 @@ const divide = function (a,b) {
 let num1;
 let num2;
 let operator;
+let snum=0;
+let mainVar=0;
 
 function operate(num1,num2,operator)
 {
@@ -34,4 +36,28 @@ function operate(num1,num2,operator)
         divide(num1,num2);
         break;
   }
+}
+for (let i = 0; i <= 9; i++) {
+    let button = document.createElement('button');
+    button.textContent = i;
+    document.querySelector('.digitButtons').appendChild(button);}
+
+let button = document.createElement('button');
+button.textContent ="=";
+document.querySelector('.opButtons').appendChild(button);
+
+let menu = document.querySelector(".digitButtons");
+menu.addEventListener('click',(event)=>
+{
+    let x = event.target.textContent
+    addScreen(x);
+})
+
+let screenB = document.querySelector(".screen");
+
+function addScreen(num){
+    snum = (snum * 10) + Number(num);
+    screenB.innerHTML=""
+    screenB.textContent=snum
+    mainVar=snum
 }
